ce9a9b1ca9c361772a2a3dd6520a85058a309e59
cassiano belniak <cassi.belniak@gmail.com>
2019-05-31 22:42:06 -0300

    Adicionado movimento basico do carro

--- Car.gd
+++ Car.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 0
+var direction = 0
+
+var max_speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +11,17 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_pressed("turn_left"):
-		add_torque(-30)
+		direction -= 0.1 * (speed/max_speed)
 	if Input.is_action_pressed("turn_right"):
-		add_torque(30)
+		direction += 0.1 * (speed/max_speed)
 	if Input.is_action_pressed("acelerate"):
-		add_force(Vector2(0,0), Vector2(10,0))
+		speed += 0.5
+	else:
+		if speed > 0:
+			speed *= 0.99
+			
+	if speed > max_speed:
+		speed = max_speed
+		
+	linear_velocity = Vector2(speed,0).rotated(direction)
+	rotation = direction
