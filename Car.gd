7d604199a687bb447d0400537dd9d366ebb3c950
cassiano belniak <cassi.belniak@gmail.com>
2019-06-02 12:30:55 -0300

    Changed some of the physics

--- Car.gd
+++ Car.gd
@@ -5,7 +5,7 @@ onready var area = $Area2D
 var speed = 0
 var direction = 0
 var is_rewind = false
-var max_speed = 300
+var max_speed = 100
 onready var is_on_table = 10
 
 func _ready():
@@ -13,36 +13,25 @@ func _ready():
 
 
 func _physics_process(delta):
-	
-	if Input.is_action_pressed("acelerate"):
-		if linear_velocity.length() < max_speed:
-			apply_central_impulse(Vector2(10,0).rotated(rotation))
-		is_rewind = false
-	elif Input.is_action_pressed("break"):
-		apply_central_impulse(Vector2(10,0).rotated(rotation - PI))
-		is_rewind = true
-	else:
-		apply_central_impulse(-linear_velocity*0.01)
-		
-		
-		
-	var turning_factor = (linear_velocity.length() if linear_velocity.length() < max_speed else max_speed) /(max_speed/1.1)
-	if Input.is_action_pressed("turn_left"):
-		apply_torque_impulse(-30*turning_factor)
-	if Input.is_action_pressed("turn_right"):
-		apply_torque_impulse(30*turning_factor)
-		
-	
 	if is_on_table > 0:
-		var force = linear_velocity
-		apply_central_impulse(-force)
-		var diference
-		if is_rewind:
-			diference = angle_to_angle(rotation - PI, force.angle())
-		else:
-			diference = angle_to_angle(rotation, force.angle())
-		apply_central_impulse(force.rotated(-diference))
-	
+		if Input.is_action_pressed("acelerate"):
+			apply_central_impulse(Vector2(5,0).rotated(rotation))
+			is_rewind = false
+		elif Input.is_action_pressed("break"):
+			apply_central_impulse(Vector2(5,0).rotated(rotation - PI))
+			is_rewind = true
+			
+		if abs(get_linear_velocity().x) > max_speed or abs(get_linear_velocity().y) > max_speed:
+			var new_speed = get_linear_velocity().normalized()
+			new_speed *= max_speed
+			set_linear_velocity(new_speed)
+			
+			
+		if Input.is_action_pressed("turn_left"):
+			apply_torque_impulse(-20)
+		if Input.is_action_pressed("turn_right"):
+			apply_torque_impulse(20)
+		
 	var check = true
 	for a in area.get_overlapping_areas():
 		if a.is_in_group("Ground"):
@@ -55,8 +44,11 @@ func _physics_process(delta):
 	if is_on_table <= 0:
 		z_index = -1
 		gravity_scale = 2
-	
+		
 	
 	
 static func angle_to_angle(from, to):
     return fposmod(to-from + PI, PI*2) - PI
+
+func _on_Car_body_entered(body):
+	pass
--- EnemyCar.gd
+++ EnemyCar.gd
@@ -10,46 +10,34 @@ onready var is_on_table = 10
 var target = null
 
 func _physics_process(delta):
-	if target == null:
-		target = find_target()
-		
-	if true:
-		if linear_velocity.length() < max_speed:
-			apply_central_impulse(Vector2(10,0).rotated(rotation))
-		is_rewind = false
-	else:
-		apply_central_impulse(-linear_velocity*0.01)
-		
-	var avoid = find_avoid()
-		
-	var d
-	if avoid == null:
-		var angle1 = rad2deg(global_position.angle_to_point(target.global_position))
-		var angle2 = rotation_degrees
-		d = angle_difference(angle1, angle2)
-	else:
-		var angle1 = rad2deg(global_position.angle_to_point(target.global_position))
-		var angle2 = rotation_degrees
-		d = angle_difference(angle1, angle2) -180
-	
-	var turning_factor = (linear_velocity.length() if linear_velocity.length() < max_speed else max_speed) /(max_speed/1.1)
-	
-	if d < 0:
-		apply_torque_impulse(-30*turning_factor)
-	else:
-		apply_torque_impulse(30*turning_factor)
-		
-	
 	if is_on_table > 0:
-		var force = linear_velocity
-		apply_central_impulse(-force)
-		var diference
-		if is_rewind:
-			diference = angle_to_angle(rotation - PI, force.angle())
+		if target == null:
+			target = find_target()
+			
+		apply_central_impulse(Vector2(5,0).rotated(rotation))
+		
+		if abs(get_linear_velocity().x) > max_speed or abs(get_linear_velocity().y) > max_speed:
+			var new_speed = get_linear_velocity().normalized()
+			new_speed *= max_speed
+			set_linear_velocity(new_speed)
+			
+		var avoid = find_avoid()
+			
+		var d
+		if avoid == null:
+			var angle1 = rad2deg(global_position.angle_to_point(target.global_position))
+			var angle2 = rotation_degrees
+			d = angle_difference(angle1, angle2)
 		else:
-			diference = angle_to_angle(rotation, force.angle())
-		apply_central_impulse(force.rotated(-diference))
+			var angle1 = rad2deg(global_position.angle_to_point(target.global_position))
+			var angle2 = rotation_degrees
+			d = angle_difference(angle1, angle2) -180
 	
+		if d < 0:
+			apply_torque_impulse(-20)
+		else:
+			apply_torque_impulse(20)
+		
 	var check = true
 	for a in area.get_overlapping_areas():
 		if a.is_in_group("Ground"):
@@ -62,6 +50,7 @@ func _physics_process(delta):
 	if is_on_table <= 0:
 		z_index = -1
 		gravity_scale = 2
+
 	
 func find_target():
 	var list = get_tree().get_nodes_in_group("Target")
